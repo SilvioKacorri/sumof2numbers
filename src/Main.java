288f804f2983e953b43289b7889676a761bd40c5
SilvioKacorri <s.kacorri@calmcoders.net>
2025-03-05 17:37:53 +0100

    Fixed implementation

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> numra = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24));
-        test(numra,20);
+        ArrayList<Integer> numra = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24));
+        HashMap<ArrayList<Integer>, Integer> integerHashMap  = test(numra,20);
+        System.out.println(integerHashMap);
+        System.out.println(test(numra,10));
     }
-    public static HashMap<Integer,Integer> test(ArrayList<Integer> list, int j){
+    public static HashMap<ArrayList<Integer>,Integer> test(ArrayList<Integer> list, int j){
 
-        Map<ArrayList , Integer>  integerHashMap = new HashMap<>();
+        HashMap<ArrayList<Integer> , Integer>  integerHashMap = new HashMap<>();
 
         for (Integer i : list){
             for (Integer k : list){
@@ -21,7 +23,6 @@ public class Main {
                 }
             }
         }
-        System.out.println(integerHashMap);
-        return new HashMap<Integer, Integer>();
+        return integerHashMap;
     }
 }
